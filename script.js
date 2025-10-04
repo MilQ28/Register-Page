0d509c57d9b850d53cc7f5094bad8fa9d9603809
Syamil CA <65897749+MilQ28@users.noreply.github.com>
2025-10-04 08:06:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function reset() {
+    document.getElementById("input").value = "";
+    document.getElementById("input2").value = "";
+    document.getElementById("input3").value = "";
+    document.getElementById("input4").value = "";
+}
